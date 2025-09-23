5e655b719caa81e6d5e8be5aa91299920e8058d0
Aldahir2006 <2430271@upv.edu.mx>
2025-09-22 23:04:30 -0600

    Lo intente otra vez pero no funciono aqui va el siguiente

--- main.cpp
+++ main.cpp
@@ -1,4 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <limits>
+
+Hola lo inteto una vez mas ayudame por favor
+
+const int MAX_ATLETAS = 100;
+std::string nombres[MAX_ATLETAS];
+std::string paises[MAX_ATLETAS];
+std::string disciplinas[MAX_ATLETAS];
+char generos[MAX_ATLETAS];
+int medallas[MAX_ATLETAS];
+int n_atletas = 0;
+
+bool cargarDatos(const std::string& ruta);
+int buscarAtleta(const std::string& nombre);
+int totalMedallasPorPais(const std::string& pais);
+int atletaConMasMedallas();
+void mostrarAtleta(int indice);
+void limpiarEntrada();
+
+int main(int argc, char* argv[]) {
+    std::string rutaArchivo;
+
+    if (argc > 1)
+        rutaArchivo = argv[1];
+    else
+        std::getline(std::cin, rutaArchivo);
+
+    if (!cargarDatos(rutaArchivo))
+        return 1;
 
 
 int main() {
